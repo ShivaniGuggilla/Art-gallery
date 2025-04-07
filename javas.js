0eccad44d7340aa5efff0f3bf9e7f030872e133c
ShivaniGuggilla <guggillashivani123@gmail.com>
2025-04-07 11:14:42 +0530

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,40 @@
+function classicp1()
+{
+    window.location.href="classicp2.html"
+}
+function classicp2()
+{
+    window.location.href="classicp1.html"
+}
+function aestheticp1()
+{
+    window.location.href="aestheticp2.html"
+}
+function aestheticp2()
+{
+    window.location.href="aestheticp1.html"
+}  
+function darkp1()
+{
+    window.location.href="darkp2.html"
+}
+function darkp2()
+{
+    window.location.href="darkp1.html"
+}
+function entry()
+{
+    window.location.href="entry.html"
+}
+function entry1()
+{
+    window.location.href="aestheticp1.html"
+}
+function entry2()
+{
+    window.location.href="classicp1.html"
+}
+function entry3()
+{
+    window.location.href="darkp1.html"
+}
